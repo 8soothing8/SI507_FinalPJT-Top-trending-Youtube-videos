6a9493a471b62e01c278666f80f0587329fc77af
8soothing8 <46687925+8soothing8@users.noreply.github.com>
2019-04-25 17:39:03 -0400

    final submission

--- SI507project_tests.py
+++ SI507project_tests.py
@@ -1,38 +1,25 @@
-
-from SI507project_tools import *
-import types
 import unittest
+import sqlite3
+from SI507project_tools import *
 
-data_call_obj = call_mostpopular_video()
-list = []
+class Cache(unittest.TestCase):
+    def test_cache_diction(self):
+        self.assertIsInstance(CACHE_DICTION, dict,"Cached file is a dictionary type")
+    def test_cache_contents(self):
+        testfile = open("SI507finalproject_cached_data.json","r")
+        testfilestr = testfile.read()
+        testfile.close()
+        self.assertTrue(len(testfilestr)>0, "Cached file contains results")
 
-class Test_stuff(unittest.TestCase):
-    def setup(self):
-        self.object = data_call_obj
+class APIcall(unittest.TestCase):
+    def test_api_result(self):
+        self.assertIsInstance(popular_video_list, list, "Newly called file is a list type")
 
-# Test 1: Check data was called
-    def test_datatype(self):
-        self.assertTrue(type(self.object), list)
+class DBTests(unittest.TestCase):
+    def setUp(self):
+        self.conn = sqlite3.connect("youtube_popular_videos.db")
+        self.cur = self.conn.cursor()
 
-if __name__ == '__main__':
-    unittest.main()
-    #
-    #
-    # if type(data_call_obj) is type(list):
-    #     pass
-    # else:
-    #     print("Error: Video information is not fetched")
 
-# Test 2:
-# what = DB_setup()
-# print(Vidoe_class())
-
-suite = unittest.TestSuite()
-# suite.addTest(Introduction('test_case1'))
-print(suite)
-unittest.TextTestRunner().run(suite)
-
-# if isinstance(DB,  ):
-#     pass
-# else:
-#     print("Error: Video information is not fetched")
+if __name__ == "__main__":
+    unittest.main(verbosity=3)
--- SI507project_tools.py
+++ SI507project_tools.py
@@ -2,22 +2,34 @@
 import codecs
 import sys
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer)
-
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
 import csv
 import json
-
-import os
-import sys
 from sqlalchemy import Table, Column, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-
 import sqlite3
+import os
+from flask import (Flask, request, render_template, session, redirect, url_for, Response) # It should be inside a paranthesis?
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.debug = True
+app.use_reloader = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///youtube_popular_videos.db'
+# Q1. Should it be sqlite or .db?
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+session = db.session
 
 DEVELOPER_KEY = "AIzaSyCjGtYNYaE8-CI0nYbNrrMDfBTFbtaeaIY"
 #"AIzaSyAKlh8L9MNnZg-O75TkBxKSpQk0IorI1Vs"
@@ -26,19 +38,72 @@ YOUTUBE_API_VERSION = "v3"
 
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
 
-def call_mostpopular_video():
 
-    # Call the videos.list method to retrieve video info
-    list_videos_by_id = youtube.videos().list(
-        part = "id, snippet, contentDetails, statistics",
-                  chart ='mostPopular', regionCode ='DE',
-           maxResults = 10, videoCategoryId ='').execute()
+# Q1. It doesn't create the cached file
+
+@app.route('/')
+def welcome():
+    return render_template("welcome.html")
+
+# RECIEVE INPUT FROM A USER
+@app.route('/country')
+def choose_country():
+    country_codes = ["US", "GB", "IN", "DE", "CA", "FR", "KR", "RU", "JP", "BR", "MX"]
+    #
+    # if request.method == 'GET':
+    #     code = request.args.get(country)
+    return render_template("country.html", country_codes = country_codes)
+
+
+
+# DEFINE CACHING PATTERN
+CACHE_FNAME = "SI507finalproject_cached_data.json"
+
+try:
+    cache_file = open(CACHE_FNAME, 'r')
+    cache_contents = cache_file.read()
+    CACHE_DICTION = json.loads(cache_contents)
+    cache_file.close()
+except:
+    CACHE_DICTION ={}
+
+def params_unique():
+    date = datetime.now().strftime("%m/%d/%Y")
+    return date
+
+# GET DATA FROM API IF IT WAS NOT CACHED
+total_rows = []
+
+def make_cache():
+    unique_identifier = params_unique()
+    if unique_identifier in CACHE_DICTION:
+        print("Getting data from a cached file")
+        return CACHE_DICTION[unique_identifier]
 
     # extracting the results from search response
-    popular_videos = list_videos_by_id.get("items", [])
-    # print(popular_videos)
+    else:
+        list_videos_by_id = youtube.videos().list(
+            part = "id, snippet, contentDetails, statistics",
+                      chart ='mostPopular', regionCode = "US",
+               maxResults = 10, videoCategoryId ='').execute()
 
-    total_rows = []
+        popular_videos = list_videos_by_id.get("items")[0]
+        print(popular_videos)
+        popular_videos_obj = json.dumps(popular_videos)
+        cache_file_obj = open(CACHE_FNAME, 'w')
+        CACHE_DICTION[unique_identifier] = popular_videos_obj
+        cache_file_obj.write(json.dumps(CACHE_DICTION))
+        cache_file_obj.close()
+        return popular_videos_obj
+
+        # print(popular_videos)
+
+def make_call():
+    list_videos_by_id = youtube.videos().list(part = "id, snippet, contentDetails, statistics",
+                      chart ='mostPopular', regionCode = "US",
+               maxResults = 6, videoCategoryId ='').execute()
+    popular_videos = list_videos_by_id.get("items", [])
+
     for video in popular_videos:
 
         row = []
@@ -54,7 +119,7 @@ def call_mostpopular_video():
         row.append(channelId)
         channelTitle = response['items'][0]['snippet']['channelTitle']
         row.append(channelTitle)
-        # print(channelTitle)
+            # print(channelTitle)
         favoriteCount = response['items'][0]['statistics']['favoriteCount']
         row.append(favoriteCount)
         viewCount = response['items'][0]['statistics']['viewCount']
@@ -63,11 +128,11 @@ def call_mostpopular_video():
         row.append(likeCount)
         dislikeCount = response['items'][0]['statistics']['dislikeCount']
         row.append(dislikeCount)
-        # if 'commentCount' in response['items'][0]['statistics']:
-        #     commentCount = response['items'][0]['statistics']['commentCount']
-        #     row.append(commentCount)
-        # else:
-        #     row.append('NULL')
+            # if 'commentCount' in response['items'][0]['statistics']:
+            #     commentCount = response['items'][0]['statistics']['commentCount']
+            #     row.append(commentCount)
+            # else:
+            #     row.append('NULL')
         favoriteCount = response['items'][0]['statistics']['favoriteCount']
         row.append(favoriteCount)
 
@@ -75,12 +140,11 @@ def call_mostpopular_video():
 
     return total_rows
 
-#     call_mostpopular_video()
-#
-popular_video_list = call_mostpopular_video()
+popular_video_list = make_call()
+# print('***This is where I need to check*** ',popular_video_list)
 
-# print(popular_video_list)
 
+# SET UP DATABASE
 
 Base = declarative_base()
 session = scoped_session(sessionmaker())
@@ -105,11 +169,12 @@ def DB_setup():
 
 class Video_class(Base):
     __tablename__ = 'Video'
-    videoId = Column(String(250), primary_key = True, nullable = False)
+    videoId = Column(String(250),ForeignKey('Reaction.videoId'), primary_key = True, nullable = False)
     title = Column(String(250), nullable = False)
     channelId = Column(String(250), ForeignKey('Channel.channelId'))
 
     channel = relationship("Channel_class")
+    reaction = relationship("Reaction_class")
 
 # video_test = Video_class()
 
@@ -125,18 +190,26 @@ class Channel_class(Base):
 class Reaction_class(Base):
     __tablename__ = 'Reaction'
     videoId = Column(String(250), primary_key = True, nullable = False)
+    videoTitle = Column(String(250), nullable = False)
     viewCount = Column(Integer(), nullable = False)
     likeCount = Column(Integer(), nullable = False)
     dislikeCount = Column(Integer(), nullable = False)
-    favoriteCount = Column(Integer(), nullable = False)
+
+    video = relationship('Video_class', backref = "Reaction_class")
+
+
+#    favoriteCount = Column(Integer())
 #    commentCount = Column(Integer())
 
-reaction_test = Reaction_class()
+# reaction_test = Reaction_class()
 
 if __name__ == "__main__":
     DB_setup()
     print("Database created")
 
+print(popular_video_list)
+# for video in popular_video_list:
+#     print(popular_video_list[1])
 
 def DB_stowing():
 
@@ -144,7 +217,8 @@ def DB_stowing():
         videoId = video[0]
         title = video[1]
         channelId = video[2]
-
+        if session.query(Video_class).filter_by(videoId = videoId).first():
+            continue
         video_inst = Video_class(videoId = videoId, title=title, channelId = channelId)
         session.add(video_inst)
 
@@ -153,21 +227,24 @@ def DB_stowing():
     for video in popular_video_list:
         channelId = video[2]
         channelTitle = video[3]
-
+        if session.query(Channel_class).filter_by(channelId = channelId ).first():
+            continue
         channel_inst = Channel_class(channelId = channelId, channelTitle=channelTitle)
-
         session.add(channel_inst)
 
     session.commit()
 
     for video in popular_video_list:
         videoId = video[0]
+        videoTitle = video[1]
         viewCount = video[5]
         likeCount = video[6]
         dislikeCount = video[7]
         favoriteCount = video[8]
+        if session.query(Reaction_class).filter_by(videoId = videoId).first():
+            continue
 
-        reaction_inst = Reaction_class(videoId=videoId, viewCount=viewCount, likeCount=likeCount, dislikeCount=dislikeCount, favoriteCount=favoriteCount) #commentCount=commentCount)
+        reaction_inst = Reaction_class(videoId=videoId,videoTitle=videoTitle, viewCount=viewCount, likeCount=likeCount, dislikeCount=dislikeCount) #favoriteCount=favoriteCount) #commentCount=commentCount)
 
         session.add(reaction_inst)
 
@@ -177,3 +254,34 @@ if __name__ == "__main__":
     DB_stowing()
     print("Database filled with video info")
     # print(type(DB_setup())) : <class 'sqlalchemy.engine.base.Engine'>
+
+
+@app.route('/result')
+def show_video():
+    videos_bycountry = []
+    videos = session.query(Video_class).all()
+    for v in videos:
+        videos_bycountry.append(v.title)
+    return render_template("result.html", videos_bycountry = videos_bycountry)
+
+@app.route('/reaction')
+def show_reaction():
+    reaction_tovideo = []
+    videos = session.query(Reaction_class).all()
+    for v in videos:
+        reaction_tovideo.append((v.videoTitle, v.viewCount, v.likeCount, v.dislikeCount))
+    return render_template("reaction.html", reaction_tovideo = reaction_tovideo)
+
+@app.route('/channel')
+def show_channel():
+    channel_ofvideo = []
+    channels = session.query(Channel_class).all()
+    for c in channels:
+        channel_ofvideo.append(c.channelTitle)
+
+    return render_template("channel.html", channel_ofvideo = channel_ofvideo)
+
+
+
+if __name__ == '__main__':
+    app.run()
